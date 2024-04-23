1d4075f204a3d86ccfca38cb72c77d2c18b0f990
FreezingEYES-Singh <151192375+FreezingEYES-Singh@users.noreply.github.com>
2024-04-23 16:20:40 +0000

    Pending changes exported from your codespace

--- login.php
+++ login.php
@@ -1,26 +1,26 @@
-<?php 
-include "config.php";
+ <?php 
+// include "config.php";
 
-if(isset($_POST["submit"])){
-    $usernameemail = $_POST["usernameemail"];
-    $password = $_POST["password"];
-    $result = mysqli_query($conn,"SELECT * FROM db_user WHERE username = '$usernameemail' OR email = '$usernameemail'");
-    $row = mysqli_fetch_assoc($result);
-    if(mysqli_num_rows($result) > 0){
-        if($password == $row["password"]){
-            $_SESSION["login"] = true;
-            $_SESSION["id"] = $row["id"];
-            header("Location: index.php");
-        } else {
-            echo
-            "<script> alert('Wrong Password'); </script>";
-        }
-    } else{
-        echo
-        "<script> alert('Username not registered'); </script>";
-    }
-}
-?>
+// if(isset($_POST["submit"])){
+//     $usernameemail = $_POST["usernameemail"];
+//     $password = $_POST["password"];
+//     $result = mysqli_query($conn,"SELECT * FROM db_user WHERE username = '$usernameemail' OR email = '$usernameemail'");
+//     $row = mysqli_fetch_assoc($result);
+//     if(mysqli_num_rows($result) > 0){
+//         if($password == $row["password"]){
+//             $_SESSION["login"] = true;
+//             $_SESSION["id"] = $row["id"];
+//             header("Location: index.php");
+//         } else {
+//             echo
+//             "<script> alert('Wrong Password'); </script>";
+//         }
+//     } else{
+//         echo
+//         "<script> alert('Username not registered'); </script>";
+//     }
+// }
+?> 
 
 
 <!DOCTYPE html>
